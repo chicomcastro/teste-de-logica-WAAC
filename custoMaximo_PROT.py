4e3667b3208f7437c36cc20833dba556cb389d79
Francisco Castro <Franciscommcastro@gmail.com>
2019-11-15 19:58:47 -0300

    Update custoMaximo_PROT.py
    Protótipo de solução nro 2

--- custoMaximo_PROT.py
+++ custoMaximo_PROT.py
@@ -1,22 +1,25 @@
-# Protótipo inicial a solução do problema
+# Titulo: Prototipo melhorado da solucao para o problema
+# Descricao: Solucao forca bruta para o problema proposto.
+#
 # Autor: Francisco Castro
 # Data: 15/11/2019
 # Codigo desenvolvido em Python 3.7.0 32-bit
-#
-# Esta solução não foi baseada em força bruta como se propôs e também resolve apenas
-# um subconjunto do espaço amostral.
 
 teste = input()
 
 initialCost = teste[0][0]
 
 def maxCost(triangle, maxCostUntilNow, i, j):
-    print(i,j)
+    print(i,j, maxCostUntilNow)
     if i >= len(triangle)-1 or j >= len(triangle)-1:
         return maxCostUntilNow
-    if triangle[i+1][j] > triangle[i+1][j+1]:
-        return maxCost(triangle, maxCostUntilNow + triangle[i+1][j],i+1,j)
+    
+    firstWay = maxCost(triangle, maxCostUntilNow + triangle[i+1][j],i+1,j)
+    secondWay = maxCost(triangle, maxCostUntilNow + triangle[i+1][j+1],i+1,j+1)
+
+    if(firstWay > secondWay):
+        return firstWay
     else:
-        return maxCost(triangle, maxCostUntilNow + triangle[i+1][j+1],i+1,j+1)
+        return secondWay
 
 print(maxCost(teste, initialCost, 0, 0))
